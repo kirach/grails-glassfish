491b6f5ddb42266906bdc4b545086a0d4f85a541
kiRach <kirach@kirach.net>
2012-04-03 18:24:10 +0400

    Updated functionality. Comet support.

--- src/groovy/net/kirach/grails/plugin/glassfish/GlassfishEmbeddableServer.groovy
+++ src/groovy/net/kirach/grails/plugin/glassfish/GlassfishEmbeddableServer.groovy
@@ -1,21 +1,16 @@
 package net.kirach.grails.plugin.glassfish
 
+import static grails.build.logging.GrailsConsole.instance as CONSOLE
+
+import grails.util.BuildSettings
+import grails.util.BuildSettingsHolder
 import grails.web.container.EmbeddableServer
 import org.glassfish.embeddable.GlassFish
-import org.glassfish.embeddable.GlassFishProperties
-import org.glassfish.embeddable.BootstrapProperties
 import org.glassfish.embeddable.GlassFishRuntime
 import org.glassfish.embeddable.archive.ScatteredArchive
-import org.glassfish.embeddable.web.WebContainer
-import org.glassfish.embeddable.Deployer
-import org.glassfish.embeddable.web.WebListener
-import org.glassfish.embeddable.web.HttpListener
 import org.glassfish.embeddable.web.Context
-import org.glassfish.embeddable.web.VirtualServer
-import org.glassfish.embeddable.web.config.VirtualServerConfig
-import grails.util.BuildSettingsHolder
+import org.glassfish.embeddable.web.WebContainer
 import org.glassfish.embeddable.web.config.WebContainerConfig
-import java.util.logging.Level
 
 /**
  * Glassfish embeddable server for grails use.
@@ -23,6 +18,8 @@ import java.util.logging.Level
  * @author kiRach
  */
 class GlassfishEmbeddableServer implements EmbeddableServer {
+
+	final BuildSettings buildSettings
 
 	GlassFish glassfish
 	WebContainer embedded
@@ -37,70 +34,79 @@ class GlassfishEmbeddableServer implements EmbeddableServer {
 	 * Constructor.
 	 */
 	GlassfishEmbeddableServer(String basedir, String webXml, String contextPath, ClassLoader classLoader) {
-		//just remember this params for using later
+		this.buildSettings = BuildSettingsHolder.getSettings()
 		this.glassfish = GlassFishRuntime.bootstrap().newGlassFish();
+
+		//just remember this params for using later
 		this.webXml = webXml
 		this.basedir = basedir
 		this.contextPath = contextPath
 		this.classLoader = classLoader
 	}
 
-	private doStart() {
-		System.out.println("Starting a GlassFish server")
+	private doStart(String host, int httpPort) {
+		CONSOLE.updateStatus "Starting GlassFish server"
 
 		//start server
-		glassfish.start();
+		this.glassfish.start();
 
-		embedded = glassfish.getService(WebContainer.class);
+		this.embedded = glassfish.getService(WebContainer.class);
 
 		WebContainerConfig config = new WebContainerConfig();
 		//here we can configure our web container
-		config.setPort(8080)
-		embedded.setConfiguration(config);
+		config.setPort(httpPort)
+		config.setHostNames(host)
+		this.embedded.setConfiguration(config);
+
+		if (getConfigParam("comet")) {
+			CONSOLE.updateStatus "Enabling GlassFish Comet support"
+			def commandRunner = glassfish.getCommandRunner()
+			def res = commandRunner.run("set", "server-config.network-config.protocols.protocol." + config.getListenerName() + ".http.comet-support-enabled=true")
+		}
 
 		//let's create scattered archive - representation of our web-application
 		//but we won't use it by the way described in documentation - we need it for creating custom context
-		ScatteredArchive archive = new ScatteredArchive(this.contextPath, ScatteredArchive.Type.WAR, new File(basedir))
-		archive.addMetadata(new File(webXml));
+		ScatteredArchive archive = new ScatteredArchive(this.contextPath, ScatteredArchive.Type.WAR, new File(this.basedir))
+		archive.addMetadata(new File(this.webXml));
 
 		//let's create context - our web application from scattered war from step before and ClassLoader, provided by grails
-		this.context = embedded.createContext(new File(archive.toURI()), this.contextPath, this.classLoader)
+		this.context = this.embedded.createContext(new File(archive.toURI()), this.contextPath, this.classLoader)
 
 	}
 
 	@Override
 	void start() {
-		doStart()
+		start(null, null)
 	}
 
 	@Override
 	void start(int port) {
-		doStart()
+		start(null, port)
 	}
 
 	@Override
 	void start(String host, int port) {
-		doStart()
+		doStart(host ?: DEFAULT_HOST, port ?: DEFAULT_PORT)
 	}
 
 	@Override
 	void startSecure() {
-		//To change body of implemented methods use File | Settings | File Templates.
+		//TODO
 	}
 
 	@Override
 	void startSecure(int port) {
-		//To change body of implemented methods use File | Settings | File Templates.
+		//TODO
 	}
 
 	@Override
 	void startSecure(String host, int httpPort, int httpsPort) {
-		//To change body of implemented methods use File | Settings | File Templates.
+		//TODO
 	}
 
 	@Override
 	void stop() {
-		System.out.println("Stopping server " + glassfish);
+		CONSOLE.updateStatus "Stopping GlassFish server"
 		if (glassfish != null) {
 			glassfish.stop();
 			glassfish.dispose();
@@ -110,9 +116,13 @@ class GlassfishEmbeddableServer implements EmbeddableServer {
 
 	@Override
 	void restart() {
-		System.out.println("Restarting a GlassFish server")
-		//here it mentioned, that restart doesn't work: http://bit.ly/Hhb9nw
-		glassfish.stop()
-		glassfish.start()
+		CONSOLE.updateStatus "Restarting a GlassFish server"
+		//here it is mentioned, that restart doesn't work: http://bit.ly/Hhb9nw
+		stop()
+		start()
+	}
+
+	private getConfigParam(String name) {
+		buildSettings.config.grails.glassfish[name]
 	}
 }
