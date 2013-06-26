30cb1e54d36d5388d2416715ba49ec9d6577459d
Burt Beckwith <burt@burtbeckwith.com>
2013-06-26 13:47:44 -0400

    cleanup

--- GlassfishGrailsPlugin.groovy
+++ GlassfishGrailsPlugin.groovy
@@ -1,76 +1,23 @@
 class GlassfishGrailsPlugin {
-	// the plugin version
 	def version = "0.1.2"
-	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "2.0 > *"
-	// the other plugins this plugin depends on
-	def dependsOn = [:]
-	// resources that are excluded from plugin packaging
 	def pluginExcludes = [
-			"grails-app/views/error.gsp",
-			"grails-app/views/index.gsp",
-			"grails-app/views/test/index.gsp",
-			"grails-app/controllers/glassfish/TestController.groovy",
-			"web-app/js/application.js",
-			"web-app/test.gsp",
+		"grails-app/controllers/**",
+		"grails-app/views/**",
+		"web-app/**",
 	]
 
 	// don't package in war
 	def scopes = [excludes: 'war']
 
-	def title = "Glassfish Plugin" // Headline display name of the plugin
+	def title = "Glassfish Plugin"
 	def author = "kiRach"
 	def authorEmail = "kirach@kirach.net"
-	def description = 'Plugin for developing grails applications under glassfish app-server.'
+	def description = 'Plugin for developing Grails applications under Glassfish app-server.'
 
-	// URL to the plugin's documentation
 	def documentation = "https://github.com/kiRach/grails-glassfish"
 
-	// Extra (optional) plugin metadata
-
-	// License: one of 'APACHE', 'GPL2', 'GPL3'
 	def license = "APACHE"
-
-	// Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-	// Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-	// Location of the plugin's issue tracker.
 	def issueManagement = [system: "GITHUB", url: "https://github.com/kiRach/grails-glassfish/issues"]
-
-	// Online location of the plugin's browseable source code.
 	def scm = [url: "https://github.com/kiRach/grails-glassfish"]
-
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional), this event occurs before
-	}
-
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}
-
-	def doWithDynamicMethods = { ctx ->
-		// TODO Implement registering dynamic methods to classes (optional)
-	}
-
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)
-	}
-
-	def onChange = { event ->
-		// TODO Implement code that is executed when any artefact that this plugin is
-		// watching is modified and reloaded. The event contains: event.source,
-		// event.application, event.manager, event.ctx, and event.plugin.
-	}
-
-	def onConfigChange = { event ->
-		// TODO Implement code that is executed when the project configuration changes.
-		// The event is the same as for 'onChange'.
-	}
-
-	def onShutdown = { event ->
-		// TODO Implement code that is executed when the application shuts down (optional)
-	}
 }
